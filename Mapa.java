270f0cea635ec6885b560e4cbcad4f81829e0a60
mand027 <mando27tuzo@gmail.com>
2016-10-26 11:28:30 -0500

    Update Mapa.java

--- Mapa.java
+++ Mapa.java
@@ -1,4 +1,45 @@
-public class Mapa
-{
+public class Mapa {
+	private int largo;
+	private int ancho;
+	private int[][] numeroDeCAsilla;
+	private double probDeCaja;
+	private boolean casillaExplorasa;
+	
 	public Mapa(){ }
+	
+	public int getLargo(){
+		return largo; 
+	}
+	public void setLargo(int largo){
+		this.largo = largo;
+	}
+	public int getAncho(){
+		return ancho;
+	}
+	public void setAncho(int ancho){
+		this.ancho = ancho;
+	}
+	public double getProbDeCaja(){
+		return probDeCaja; 
+	}
+	public void setProbDeCaja(double probDeCaja){
+		this.probDeCaja = probDeCaja; 
+	}
+	
+	public boolean getCasillaExplorada(){
+		return casillaExplorada;
+	}
+	
+	public void setCasillaExplorada(boolean casillaExplorada){
+		this.casillaExplorada = casillaExplorada;
+	}
+	
+	public int[][] getNumeroDeCasilla(){
+		return numeroDeCasilla;
+	}
+	
+	public void setNumeroDeCasilla(int[][]  numeroDeCasilla){
+		this.numeroDeCasilla = numeroDeCasilla;
+	}
+	
 }
